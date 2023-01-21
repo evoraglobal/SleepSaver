9743c204446b1e4e8542e7de824a0cdd6dae0ffa
ben <ben.burdsall@gmail.com>
2023-01-21 14:53:39 +0000

    update the lambda name

--- package.sh
+++ package.sh
@@ -4,4 +4,4 @@ cp *.py ./zippackage
 cd zippackage
 zip -r ../sleepsaverlambda.zip .
 cd ..
-aws lambda update-function-code --function-name sleepsaver --zip-file fileb://sleepsaverlambda.zip --region eu-west-2 --no-paginate
+aws lambda update-function-code --function-name SleepSaverProd --zip-file fileb://sleepsaverlambda.zip --region eu-west-2 --no-paginate
