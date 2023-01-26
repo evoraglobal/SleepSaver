51a83565a7338ce927b203cdf8cd23533ffda66a
ben <ben.burdsall@gmail.com>
2023-01-26 15:29:19 +0000

    fixed typo

--- CF/createSleepsaverLambda.sh
+++ CF/createSleepsaverLambda.sh
@@ -1,5 +1,6 @@
 aws s3 sync . s3://evg-sleepsaver-cf --region eu-west-2
-
-
+cd ..
+source package.sh
+cd CF
 aws cloudformation deploy --stack-name SleepSaverLambdav2 --template-file ./sleepsaverlambda.yml --region eu-west-2 --capabilities CAPABILITY_NAMED_IAM
 
--- CF/describepolicy.sh
+++
@@ -1,1 +0,0 @@
-aws iam get-policy-version --policy-arn arn:aws:iam::307494535005:policy/ecsReaderWriter --version-id v2
