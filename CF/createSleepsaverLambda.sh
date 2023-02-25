0a71f1c2a2b4d78fc9de209b258faa9b1190608b
ben <ben.burdsall@gmail.com>
2023-02-25 16:30:34 +0000

    debugged - with sub

--- CF/createSleepsaverLambda.sh
+++ CF/createSleepsaverLambda.sh
@@ -1,6 +1,4 @@
 aws s3 sync . s3://evg-sleepsaver-cf --region eu-west-2
-cd ..
-source package.sh
-cd CF
+aws s3 cp ../sleepsaverlambda.zip s3://evg-sleepsaver-cf/
 aws cloudformation deploy --stack-name SleepSaverLambdav2 --template-file ./sleepsaverlambda.yml --region eu-west-2 --capabilities CAPABILITY_NAMED_IAM
 
