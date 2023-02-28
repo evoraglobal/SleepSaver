342075d752a61bad4064d601cc3d89d0c4233ca2
ben <ben.burdsall@gmail.com>
2023-02-28 15:03:10 +0000

    sleepsaver bucket externalised

--- CF/createSleepsaverLambda.sh
+++ CF/createSleepsaverLambda.sh
@@ -1,4 +1,8 @@
-aws s3 sync . s3://evg-sleepsaver-cf --region eu-west-2
+# Set the location of the Code Bucket in S3. This needs to be set in SleepSaverLambda.yml as the CodeBucket Parameter
+export CODEBUCKET=evg-sleepsaver-cf
+
+aws s3 sync . s3://$CODEBUCKET --region eu-west-2
 aws s3 cp ../sleepsaverlambda.zip s3://evg-sleepsaver-cf/
 aws cloudformation deploy --stack-name SleepSaverLambdav2 --template-file ./sleepsaverlambda.yml --region eu-west-2 --capabilities CAPABILITY_NAMED_IAM
+
 
--- CF/createStepF.sh
+++
@@ -1,3 +0,0 @@
-aws s3 cp stepsyntax.json s3://evg-sleepsaver-cf --region eu-west-2
-
-aws cloudformation deploy --stack-name SleepSaverStepFunction --template-file ./stepfunction.yml --region eu-west-2 --capabilities CAPABILITY_NAMED_IAM
