cdf4da390acf0c03c45c5bde10dcfbb6bd4c0c33
ben <ben.burdsall@gmail.com>
2023-03-01 12:06:46 +0000

    Public release

--- CF/createSleepsaverLambda.sh
+++ CF/createSleepsaverLambda.sh
@@ -1,8 +1,9 @@
-# Set the location of the Code Bucket in S3. This needs to be set in SleepSaverLambda.yml as the CodeBucket Parameter
+# Set the location of the Code Bucket in S3. This needs to be set in SleepSaverLambda.yml and the CodeBucket Parameter
 export CODEBUCKET=evg-sleepsaver-cf
 
-aws s3 sync . s3://$CODEBUCKET --region eu-west-2
-aws s3 cp ../sleepsaverlambda.zip s3://evg-sleepsaver-cf/
-aws cloudformation deploy --stack-name SleepSaverLambdav2 --template-file ./sleepsaverlambda.yml --region eu-west-2 --capabilities CAPABILITY_NAMED_IAM
+aws s3 cp *.yml s3://$CODEBUCKET/
+aws s3 cp *.json  s3://$CODEBUCKET/
+aws s3 cp ../sleepsaverlambda.zip s3://$CODEBUCKET/
+aws cloudformation deploy --stack-name SleepSaverLambdav2 --template-file ./sleepsaverlambda.yml --s3-bucket $CODEBUCKET --region eu-west-2 --capabilities CAPABILITY_NAMED_IAM
 
 
