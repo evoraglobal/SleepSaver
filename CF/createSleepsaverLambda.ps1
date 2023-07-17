4145861b617b197374634f5befeb89f953b549ce
Alex <agraham@evoraglobal.com>
2023-07-17 12:18:25 +0100

    change cron job for NightScheduleExpression and add aws cli command

---
+++ CF/createSleepsaverLambda.ps1
@@ -0,0 +1,2 @@
+aws cloudformation deploy --stack-name SleepSaverLambdav2 --template-file ./sleepsaverlambda.yml --region eu-west-2 --capabilities CAPABILITY_NAMED_IAM --profile AWSAdministratorAccess-495799715217
+
